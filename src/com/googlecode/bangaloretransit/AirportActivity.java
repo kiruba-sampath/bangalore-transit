2d5d29e3cc0d9a43856e8e5483a9a9fec961a4c2
kirubakaran1989@gmail.com <kirubakaran1989@gmail.com@8947dee8-ccf7-c8f4-e1ea-a2052b98c8f9>
2012-04-01 19:24:56 +0000

    Completed airport shuttle part

--- src/com/googlecode/bangaloretransit/AirportActivity.java
+++ src/com/googlecode/bangaloretransit/AirportActivity.java
@@ -4,17 +4,29 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import android.app.Activity;
-import android.content.res.Resources;
-import android.content.res.XmlResourceParser;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class AirportActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
+    
+	/* variables */
+	private String from_selected = "";
+	private String to_selected = "";
+	private DBAdapter airportdb;
+	private String[] shuttle_info = new String[50];
+	private SimpleAdapter adapter;
+	private ListView airport_list;
+	
+	/** Called when the activity is first created. */
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.airport);
@@ -22,13 +34,15 @@ public class AirportActivity extends Activity {
         /* get view elements */
         Spinner from_spinner = (Spinner) findViewById(R.id.spinner_from);
         Spinner to_spinner = (Spinner) findViewById(R.id.spinner_to);
+        Button find_button = (Button) findViewById(R.id.airport_find);
+        airport_list = (ListView) findViewById(R.id.airport_list); 
         
-        DBAdapter airportdb = new DBAdapter(getApplicationContext());
+        airportdb = new DBAdapter(getApplicationContext());
         airportdb.open();
         
         
-       /* From spinner */
-       String [] from_hops = airportdb.getHops();
+       /****************** From spinner ***************/
+       String[] from_hops = airportdb.getHops();
        ArrayAdapter <CharSequence> from_adapter =
     	   new ArrayAdapter <CharSequence> (this, android.R.layout.simple_spinner_item );
        from_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -38,9 +52,20 @@ public class AirportActivity extends Activity {
     	   from_adapter.add(from_hops[gy]);
        }
        from_spinner.setAdapter(from_adapter);
+       from_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+    	   public void onItemSelected(AdapterView<?> parent, View view, int pos, long l) { 
+    		  //Toast.makeText(parent.getContext(), 
+    		  //          parent.getItemAtPosition(pos).toString(), Toast.LENGTH_LONG).show();
+    		  from_selected = parent.getItemAtPosition(pos).toString();
+    	   } 
+
+    	   public void onNothingSelected(AdapterView<?> adapterView) {
+    	        return;
+    	   }
+	   });
        
-       /* To spinner */
-       String [] to_hops = airportdb.getHops();
+       /****************** To spinner **************************/
+       String[] to_hops = airportdb.getHops();
        ArrayAdapter <CharSequence> to_adapter =
     	   new ArrayAdapter <CharSequence> (this, android.R.layout.simple_spinner_item );
        to_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -50,45 +75,48 @@ public class AirportActivity extends Activity {
     	   to_adapter.add(to_hops[gy]);
        }
        to_spinner.setAdapter(to_adapter);
+       to_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+    	   public void onItemSelected(AdapterView<?> parent, View view, int pos, long l) {
+    		  to_selected = parent.getItemAtPosition(pos).toString();
+    	   } 
+
+    	   public void onNothingSelected(AdapterView<?> adapterView) {
+    	        return;
+    	   }
+	   });
        
-       
-       /* List view */
-       ListView airport_list = (ListView) findViewById(R.id.airport_list);
-       ArrayList<HashMap<String,String>> list = new ArrayList<HashMap<String,String>>();
-       HashMap<String,String> temp = new HashMap<String,String>();
-       temp.put("pen","MONT Blanc");
-       temp.put("price", "200.00$");
-       list.add(temp);
-       HashMap<String,String> temp1 = new HashMap<String,String>();
-       temp1.put("pen","Gucci");
-       temp1.put("price", "300.00$");
-       list.add(temp1);
-       HashMap<String,String> temp2 = new HashMap<String,String>();
-       temp2.put("pen","Parker");
-       temp2.put("price", "400.00$");
-       list.add(temp2);
-       HashMap<String,String> temp3 = new HashMap<String,String>();
-       temp3.put("pen","Sailor");
-       temp3.put("price", "500.00$");
-       list.add(temp3);
-       HashMap<String,String> temp4 = new HashMap<String,String>();
-       temp4.put("pen","Porsche Design");
-       temp4.put("price", "600.00$");
-       list.add(temp4);
-       
-       SimpleAdapter adapter = new SimpleAdapter(
-    		   this,
-    		   list,
-    		   R.layout.airport_custom_row,
-    		   new String[] {"pen","price"},
-    		   new int[] {R.id.number_text,R.id.route_text});
-       
-       airport_list.setAdapter(adapter);
-       
-       /* parse the XML */
-       Resources res = getResources();
-       XmlResourceParser xpp = res.getXml(R.xml.airportshuttle);
-       
+       /*******************  Button  ************************************/   
+       find_button.setOnClickListener(new View.OnClickListener() {
+	   @Override
+	   public void onClick(View arg0) {
+            if(from_selected.equals(to_selected)) {
+            	Toast.makeText(getBaseContext(), "Get a time machine!", Toast.LENGTH_SHORT).show();
+            }
+            else {
+            shuttle_info = airportdb.getShuttles(from_selected, to_selected, true);
+            for(int jk = 0; jk < shuttle_info.length && shuttle_info[jk]!=null; jk++) {
+            	Log.d("DEBUG", shuttle_info[jk]);
+            }
+            ArrayList<HashMap<String,String>> list = new ArrayList<HashMap<String,String>>();
+            for(int jk = 0; jk < shuttle_info.length && shuttle_info[jk]!=null; jk++) {
+         	   String[] parser = shuttle_info[jk].split(";");
+         	   HashMap<String,String> temp = new HashMap<String,String>();
+         	   temp.put("route", parser[0]);
+               temp.put("board", parser[1]);
+               list.add(temp);   
+            }
+            
+            adapter = new SimpleAdapter(
+         		   getBaseContext(),
+         		   list,
+         		   R.layout.airport_custom_row,
+         		   new String[] {"route","board"},
+         		   new int[] {R.id.number_text,R.id.route_text});
+            
+            airport_list.setAdapter(adapter);
+            }
+	    }
+	   });      
     }
     
     
