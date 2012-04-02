06586a656fa919c9fe653302831aec975d6d8c34
kirubakaran1989@gmail.com <kirubakaran1989@gmail.com@8947dee8-ccf7-c8f4-e1ea-a2052b98c8f9>
2012-04-02 18:49:26 +0000

    Shuttle view completed.

--- src/com/googlecode/bangaloretransit/shuttleActivity.java
+++ src/com/googlecode/bangaloretransit/shuttleActivity.java
@@ -7,10 +7,17 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
 public class shuttleActivity extends Activity {
 	/* Variables */
 	private ListView shuttle_list;
+	private TextView route_text;
+	private TextView board_text;
+	private TextView time_text;
+	
+	private DBAdapter airportdb;
+	private int itr = 0;
 	
     /** Called when the activity is first created. */
     @Override
@@ -19,12 +26,30 @@ public class shuttleActivity extends Activity {
         setContentView(R.layout.shuttle_view);
         
         /* get view elements */
+        route_text = (TextView) findViewById(R.id.route_display);
+        board_text = (TextView) findViewById(R.id.board_display);
+        time_text = (TextView) findViewById(R.id.time_display);
         shuttle_list = (ListView) findViewById(R.id.shuttle_list);
+        
+        airportdb = new DBAdapter(getApplicationContext());
+        airportdb.open();
+        String response = airportdb.getShuttleInfo("BIAS-4", false);
+        String[] responselist = response.split("::");
+        
+        route_text.setText(responselist[itr++]);
+        board_text.setText(responselist[itr++]);
+        time_text.setText("Starts at : " + responselist[itr++]);
         
         /* List view */
         ArrayList<HashMap<String,String>> list = new ArrayList<HashMap<String,String>>();
         
-        HashMap<String,String> temp = new HashMap<String,String>();
+        while(itr < responselist.length) {	
+           HashMap<String,String> temp = new HashMap<String,String>();
+  	       temp.put("hop", responselist[itr++]);
+           temp.put("fare", responselist[itr++]);
+           list.add(temp);
+        }
+        /* temp.clear();
   	    temp.put("hop", "hop");
         temp.put("fare", "fare");
         list.add(temp);
@@ -32,12 +57,7 @@ public class shuttleActivity extends Activity {
         temp.clear();
   	    temp.put("hop", "hop");
         temp.put("fare", "fare");
-        list.add(temp);
-        
-        temp.clear();
-  	    temp.put("hop", "hop");
-        temp.put("fare", "fare");
-        list.add(temp);
+        list.add(temp); */
         
         SimpleAdapter adapter = new SimpleAdapter(
       		   getBaseContext(),
